780f1128eb55f75cda64a1868b4c4deeed2c8848
techiee3041 <doreenoms6699@gmail.com>
2024-01-11 19:12:42 +0300

    submit solution

---
+++ 0x04-TypeScript/task_5/webpack.config.js
@@ -0,0 +1,19 @@
+import { resolve as _resolve } from 'path'
+
+export const entry = '/'
+export const module = {
+  rules: [
+    {
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }
+  ]
+}
+export const resolve = {
+  extensions: ['.tsx', '.ts', '.js']
+}
+export const output = {
+  filename: 'bundle.js',
+  path: _resolve(__dirname, 'dist')
+}
